bf4c89742091492104087db3232be4de78d1a2f5
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-09-23 14:20:14 -0400

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Linq;
-
 namespace Digits
 {
     class Data
     {
-        public const string weightPath = @"C:\Users\gwflu\Desktop\Test\Digits.txt";
-        public const string biasPath = @"";
-        public static void ReadNs(NeuralNet NN, string path, bool weightorbias, int resolution)
+        public const string weightPath = @"H:\source\repos\Digits\wets.txt";
+        public const string biasPath = @"H:\source\repos\Digits\bias.txt";
+        public static void ReadNs(NeuralNet NN, bool weightorbias, string path, int resolution)
         {
             NN.Neurons = new List<Neuron>();
             FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
@@ -24,18 +18,18 @@ namespace Digits
                 {
                     int.TryParse(splitLine[1], out int result);
                     Neuron n = new Neuron(NN, 0, result, resolution);
+                    line = sr.ReadLine();
+                    splitLine = line.Split(' ');
                     for (int i = 0; i < resolution; i++)
-                    {
-                        line = sr.ReadLine();
-                        splitLine = line.Split(' ');
+                    {                       
                         try
                         {
                             double.TryParse(splitLine[i], out double result2);
                             if (weightorbias) { n.weights[i] = result2; }
                             else { n.biases[i] = result2; }
-                            
+
                         }
-                        catch (Exception ex) { Console.WriteLine(ex); }
+                        catch (Exception ex) { Console.WriteLine(ex + "\n" + i.ToString()); }
                     }
                 }
                 if (splitLine[0] == "Neuron" && splitLine[1] != "0")
@@ -43,7 +37,8 @@ namespace Digits
                     Dictionary<Neuron, double> layerwets = new Dictionary<Neuron, double>();
                     int.TryParse(splitLine[1], out int result);
                     Neuron n = new Neuron(NN, 0, result, resolution);
-                    if (result == 3) { int.TryParse(splitLine[2], out int result2); NN.Outputs[result2] = n; }
+                    int iterator1 = 0;
+                    if (result == 3) { NN.Outputs[iterator1] = n; iterator1++; }
                     line = sr.ReadLine();
                     splitLine = line.Split(' ');
 
@@ -56,15 +51,14 @@ namespace Digits
                             iterator++;
                             layerwets.Add(neuron, result2);
                         }
-                    }    
+                    }
                     foreach (KeyValuePair<Neuron, double> kvp in layerwets)
                     {
                         //May be broken
-                        if (n.layWeightBias == null) { n.layWeightBias[kvp.Key] = new double[2] { 0, 0 }; }
+                        if (!n.layWeightBias.ContainsKey(kvp.Key)) { n.layWeightBias.Add(kvp.Key, new double[2] { 0, 0 }); }
                         double[] array = n.layWeightBias[kvp.Key];
                         if (weightorbias) { array[0] = kvp.Value; }
                         else { array[1] = kvp.Value; }
-                        n.layWeightBias.Add(kvp.Key, array);
                     }
                 }
             }
@@ -77,23 +71,23 @@ namespace Digits
             foreach (Neuron n in NN.Neurons)
             {
                 sw.WriteLine("Neuron" + ' ' + n.layer);
-                if (NN.Outputs.Contains(n)) { sw.WriteLine(' ' + Array.IndexOf(NN.Outputs, n)); }
+                //if (NN.Outputs.Contains(n)) { sw.Write(' ' + Array.IndexOf(NN.Outputs, n)); }
                 if (n.layer == 0)
                 {
                     for (int i = 0; i < (resolution * resolution); i++)
                     {
-                        if (weightorbias) { sw.Write((n.weights[i]).ToString()); }
-                        else { sw.Write((n.biases[i].ToString())); }
-                        sw.WriteLine();
+                        if (weightorbias) { sw.Write((n.weights[i]).ToString() + " "); }
+                        else { sw.Write((n.biases[i].ToString()) + " "); }
                     }
+                    sw.WriteLine();
                 }
                 else
                 {
                     int count = 1;
                     foreach (KeyValuePair<Neuron, double[]> kvp in n.layWeightBias)
                     {
-                        if (weightorbias) { sw.Write((kvp.Value[0]).ToString()); }
-                        else { sw.Write(kvp.Value[1].ToString()); }  
+                        if (weightorbias) { sw.Write((kvp.Value[0]).ToString() + " "); }
+                        else { sw.Write(kvp.Value[1].ToString() + " "); }
                         count++;
                     }
                     sw.WriteLine();

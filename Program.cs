3c50012f6dd28fdfdfed39d29db77981040f4cc0
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-09-24 21:32:41 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace Digits
@@ -7,25 +7,26 @@ namespace Digits
     {
         static void Main(string[] args)
         {
+            NN nn = new NN();
+            //nn.initialize();
+            //D.WriteWeightBias(nn);
+            D.ReadWeightBias(nn);
             Reader r = new Reader();
-            int[] num = r.ReadNextLabel();
-            int[] array = r.ReadNextImage();
-            foreach (int i in num) { Console.WriteLine(i); }
-            r.PrintArray(array);
-            num = r.ReadNextLabel();
-            array = r.ReadNextImage();
-            foreach (int i in num) { Console.WriteLine("\n" + i); }
-            r.PrintArray(array);
-
-            NeuralNet nn = new NeuralNet();
-            
-            Data.ReadNs(nn, true, @"H:\Documents\wets.txt", 28);
-            Data.ReadNs(nn, false, @"H:\Documents\bias.txt", 28);
-            /*
-            nn.initNN();
-            Data.WriteNs(nn, true, @"H:\Documents\wets.txt", 28);
-            Data.WriteNs(nn, false, @"H:\Documents\bias.txt", 28);
-            */
+            while (true) { program(nn, r); }                 
+        }
+        static void program(NN nn, Reader r)
+        {           
+            int[,] image = r.ReadNextImage();
+            int correct = r.ReadNextLabel();
+            nn.backprop(image , correct);
+            nn.stochasticdescent();
+            D.WriteWeightBias(nn);
+            //Testing to ensure full copy
+            D.ReadWeightBias(nn);
+            int guess = 0; double certainty = 0;
+            //yeah, yeah, this SHOULD be a NN method, not public and used here but I'm tired right now
+            for (int i = 0; i < 10; i++) { if (nn.Outputs[i].value > certainty) { certainty = nn.Outputs[i].value; guess = i; } }
+            Console.WriteLine("Correct: " + correct.ToString() + " Guess: " + guess.ToString() + " Certainty: " + certainty);
         }
     }
 

0519866f859f702fc5a5f446b81f6aa61a745e18
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-09-27 14:20:49 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace Digits
                 {
                     active.program();
                 }
-            }                  
+            }
         }
-    
+
     }
     class active
     {
@@ -23,11 +23,11 @@ namespace Digits
         static float avg = 1;
         public static void program()
         {
-            isrunning = true;           
+            isrunning = true;
             NN nn = new NN();
-            nn.initialize();
-            D.WriteWeightBias(nn);
-            D.ReadWeightBias(nn);           
+            //nn.initialize();
+            //D.WriteWeightBias(nn);
+            D.ReadWeightBias(nn);
             double[,] image = Reader.ReadNextImage();
             int correct = Reader.ReadNextLabel();
             nn.backprop(image, correct);
@@ -37,8 +37,8 @@ namespace Digits
             //yeah, yeah, this SHOULD be a NN method, not public and used here but I'm tired right now
             for (int i = 0; i < 10; i++) { if (nn.Outputs[i].value > certainty) { certainty = nn.Outputs[i].value; guess = i; } }
             avg = (float)(avg * .99) + ((guess == correct) ? (float).01 : 0);
-            Console.WriteLine("Correct: " + correct + " Guess: " + guess + " Certainty: " + Math.Round(certainty, 5).ToString().PadRight(7) 
-                + " % Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg gradient: " + Math.Round(nn.AvgGradient, 5));
+            Console.WriteLine("Correct: " + correct + " Guess: " + guess + " Certainty: " + Math.Round(certainty, 5).ToString().PadRight(7)
+                + " % Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg gradient: " + Math.Round(nn.AvgGradient, 9));
             nn.Dispose();
             isrunning = false;
         }

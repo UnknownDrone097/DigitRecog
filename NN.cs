744403c450bdc14b9c049b40ae8cd8ef26f2f1b4
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-09-26 22:00:14 -0400

    Update NN.cs

--- NN.cs
+++ NN.cs
@@ -4,13 +4,17 @@ using System.Text;
 
 namespace Digits
 {
-    class NN
+    class NN : IDisposable
     {
         public static int Depth = 3;
         //Happens to work out that Count == OutputCount, but if this changes code will need to be refactored to account
         public static int Count = 10;
         public static int OutputCount = 10;
         public static int Resolution = 28;
+        static int LearningRate = 1;
+        static int CurrentIteration = 0;
+        static int BatchSize = 100;
+        public double AvgGradient = 0;
         //Weights/biases
         public double[,] InputWeights = new double[Count, Resolution * Resolution];
         public double[,] InputBiases = new double[Count, Resolution * Resolution];
@@ -38,15 +42,16 @@ namespace Digits
                 }
             }
         }
-        public void stochasticdescent()
+        public void Descend()
         {
             //Input layer
             for (int i = 0; i < Count; i++)
             {
                 for (int ii = 0; ii < Resolution * Resolution; ii++)
                 {
-                    InputWeights[i, ii] -= InputWeightGradient[i, ii];
-                    InputBiases[i, ii] -= InputBiasGradient[i, ii];
+                    InputWeights[i, ii] -= LearningRate * InputWeightGradient[i, ii];
+                    AvgGradient += LearningRate * InputWeightGradient[i, ii];
+                    InputBiases[i, ii] -= LearningRate * InputBiasGradient[i, ii];
                 }
             }
             //Hidden layers
@@ -56,22 +61,24 @@ namespace Digits
                 {
                     for (int iii = 0; iii < Count; iii++)
                     {
-                        HiddenWeights[i, ii, iii] -= HiddenWeightGradient[i, ii, iii];
-                        HiddenBiases[i, ii, iii] -= HiddenBiasGradient[i, ii, iii];
+                        HiddenWeights[i, ii, iii] -= LearningRate * HiddenWeightGradient[i, ii, iii];
+                        AvgGradient += LearningRate * HiddenWeightGradient[i, ii, iii];
+                        HiddenBiases[i, ii, iii] -= LearningRate * HiddenBiasGradient[i, ii, iii];
                     }
                 }
             }
+            AvgGradient /= HiddenWeightGradient.Length + InputWeightGradient.Length;
         }
-        public void backprop(int[,] image, int correct)
+        public void backprop(double[,] image, int correct)
         {
             //Run NN on image
             calculate(image);
 
             //Foreach weight/bias
-            for (int l = Depth - 2; l > -1; l--)
+            for (int l = Depth - 1; l >= 0; l--)
             {
                 //If an input layer
-                if (l == -1)
+                if (l == 0)
                 {
                     for (int k = 0; k < Count; k++)
                     {
@@ -82,28 +89,28 @@ namespace Digits
                             {
                                 upperlayerderiv += HiddenWeights[0, i, k] * Sigmoid.sigmoidderiv((InputWeights[i, k] * Neurons[1, i].value) + InputBiases[i, k]) * HiddenWeightGradient[0, i, k];
                             }
-                            double zval = (InputWeights[k, j] * image[k, j]) + InputBiases[k, j];
-                            InputWeightGradient[k, j] = image[k, j] * Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
+                            double zval = (InputWeights[k, j] * image[j / Resolution, j - ((j / Resolution) * Resolution)]) + InputBiases[k, j];                        
+                            InputWeightGradient[k, j] = Neurons[l, k].value * Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
                             InputBiasGradient[k, j] = Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
-                        }
+                        }                       
                     }
                     continue;
                 }
-                for (int k = 0; k < Count - 1; k++)
+                for (int k = 0; k < Count; k++)
                 {
-
+                    
                     //If an output layer
-                    if (l == Depth - 2)
+                    if (l == Depth - 1)
                     {
                         for (int j = 0; j < Count; j++)
                         {
-                            double zval = (HiddenWeights[l, k, j] * Neurons[l - 1, j].value) + HiddenBiases[l, k, j];
+                            double zval = (HiddenWeights[l - 1, k, j] * Neurons[l - 1, j].value) + HiddenBiases[l - 1, k, j];
 
                             int value = 0;
                             if (j == correct) { value = 1; }
                             //Formulas
-                            HiddenWeightGradient[l, k, j] = Neurons[l - 1, k].value * Sigmoid.sigmoidderiv(zval) * 2 * (Outputs[j].value - value);
-                            HiddenBiasGradient[l, k, j] = Sigmoid.sigmoidderiv(zval) * 2 * (Outputs[j].value - value);
+                            HiddenWeightGradient[l - 1, k, j] = Neurons[l, k].value * Sigmoid.sigmoidderiv(zval) * 2 * (Outputs[j].value - value);
+                            HiddenBiasGradient[l - 1, k, j] = Sigmoid.sigmoidderiv(zval) * 2 * (Outputs[j].value - value);
                         }
                         continue;
                     }
@@ -113,42 +120,64 @@ namespace Digits
                         double upperlayerderiv = 0;
                         for (int i = 0; i < Count; i++)
                         {
-                            upperlayerderiv += HiddenWeights[l + 1, i, k] * Sigmoid.sigmoidderiv((HiddenWeights[l + 1, i, k] * Neurons[l + 1, i].value) + HiddenBiases[l + 1, i, k]) * HiddenWeightGradient[l + 1, i, k];
+                            upperlayerderiv += HiddenWeights[l, i, k] * Sigmoid.sigmoidderiv((HiddenWeights[l, i, k] * Neurons[l, i].value) + HiddenBiases[l, i, k]) * HiddenWeightGradient[l, i, k];
                         }
-                        double zval = (HiddenWeights[l, k, j] * image[k, j]) + HiddenBiases[l, k, j];
-                        HiddenWeightGradient[l, k, j] = image[k, j] * Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
-                        HiddenBiasGradient[l, k, j] = Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
+                        double zval = (HiddenWeights[l - 1, k, j] * Neurons[l - 1, j].value) + HiddenBiases[l - 1, k, j];
+                        HiddenWeightGradient[l - 1, k, j] = Neurons[l, k].value * Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
+                        HiddenBiasGradient[l - 1, k, j] = Sigmoid.sigmoidderiv(zval) * upperlayerderiv;
                     }
                 }
             }
         }
-        public void calculate(int[,] image)
+        public void calculate(double[,] image)
         {
-            for (int l = 0; l < Depth - 1; l++)
+            for (int l = 0; l < Depth; l++)
             {
                 for (int k = 0; k < Count; k++)
                 {
                     //Calc input layer
                     if (l == 0)
                     {
-                        for (int j = 0; j < Count; j++)
+                        for (int j = 0; j < (Resolution * Resolution); j++)
                         {
-                            for (int jj = 0; jj < (Resolution * Resolution); jj++)
-                            {
-                                Neurons[0, k].value += (InputWeights[j, jj] * image[jj / Resolution, jj - ((jj / Resolution) * Resolution)]) + InputBiases[j, jj];                               
-                            }                           
-                        }                     
+                            Neurons[0, k].value += (InputWeights[k, j] * image[j / Resolution, j - ((j / Resolution) * Resolution)]) + InputBiases[k, j];
+                        }
                     }
                     //Calc hidden layers
                     else
                     {
                         for (int j = 0; j < Count; j++)
                         {
-                            Neurons[l, k].value += (HiddenWeights[l, k, j] * Neurons[l - 1, j].value) + HiddenBiases[l, k, j];                            
+                            Neurons[l, k].value += (HiddenWeights[l - 1, k, j] * Neurons[l - 1, j].value) + HiddenBiases[l - 1, k, j];
                         }
                     }
-                    Neurons[0, k].value = Sigmoid.sigmoid(Neurons[0, k].value);
                 }
+            }
+            //Normalize these values by layer
+            for (int i = 0; i < Depth; i++)
+            {
+                double[] array = new double[Count];
+                for (int ii = 0; ii < Count; ii++) { array[ii] = Neurons[i, ii].value; }
+                Normalize(array);
+                //This may be redundant b/c of pass by reference
+                for (int ii = 0; ii < Count; ii++) { Neurons[i, ii].value = array[ii]; }
+            }
+        }
+        public void Normalize(double[] array)
+        {
+            double mean = 0;
+            double stddev = 0;
+            //Calc mean of data
+            foreach (double d in array) { mean += d; }
+            mean /= array.Length;
+            //Calc std dev of data
+            foreach (double d in array) { stddev += (d - mean) * (d - mean); }
+            stddev /= Neurons.Length;
+            stddev = Math.Sqrt(stddev);
+            //Standardize each value with sigmoid of z-score
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Sigmoid.sigmoid((array[i] - mean) / stddev);
             }
         }
         public void initialize()
@@ -159,8 +188,9 @@ namespace Digits
             {
                 for (int ii = 0; ii < (Resolution * Resolution); ii++)
                 {
-                    InputWeights[i, ii] = (double)r.Next(-99, 99)/10000;
-                    InputBiases[i, ii] = (double)r.Next(-99, 99)/10000;
+                    //Normalize to [-5, 5]
+                    InputWeights[i, ii] = (double)r.Next(-99, 99) / 20;
+                    InputBiases[i, ii] = (double)r.Next(-99, 99) / 20;
                 }
             }
             //Initialize hidden weights/biases
@@ -170,8 +200,9 @@ namespace Digits
                 {
                     for (int iii = 0; iii < Count; iii++)
                     {
-                        HiddenWeights[i, ii, iii] = (double)r.Next(-99, 99)/10000;
-                        HiddenBiases[i, ii, iii] = (double)r.Next(-99, 99)/10000;
+                        //Normalize to [-5, 5]
+                        HiddenWeights[i, ii, iii] = (double)r.Next(-99, 99) / 15;
+                        HiddenBiases[i, ii, iii] = (double)r.Next(-99, 99) / 15;
                     }
                 }
             }
@@ -186,6 +217,42 @@ namespace Digits
                 }
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~NN()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
     class N
     {

755b134d81048d8ef7b114fc56396936da42960a
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-10-03 21:32:19 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ namespace Digits
     {
         public static bool isrunning = false;
         static float avg = 1;
+        static double avgerror = 0;
+        static int batchsize = 1;
         public static void program()
         {
             isrunning = true;
@@ -28,17 +30,28 @@ namespace Digits
             //nn.initialize();
             //D.WriteWeightBias(nn);
             D.ReadWeightBias(nn);
+            for (int i = 0; i < batchsize - 1; i++)
+            {
+                nn.backprop(Reader.ReadNextImage(), Reader.ReadNextLabel());
+                nn.Descend();
+            }
             double[,] image = Reader.ReadNextImage();
             int correct = Reader.ReadNextLabel();
             nn.backprop(image, correct);
-            nn.Descend();
+            nn.Descend(batchsize);
             D.WriteWeightBias(nn);
             int guess = 0; double certainty = 0;
             //yeah, yeah, this SHOULD be a NN method, not public and used here but I'm tired right now
             for (int i = 0; i < 10; i++) { if (nn.Outputs[i].value > certainty) { certainty = nn.Outputs[i].value; guess = i; } }
+            double error = 0;
+            for (int i = 0; i < NN.Count; i++)
+            {
+                error += ((i == correct ? 1 : 0) - nn.Outputs[i].value) * ((i == correct ? 1 : 0) - nn.Outputs[i].value);
+            }
+            avgerror = (.99 * avgerror) + (.01 * error);
             avg = (float)(avg * .99) + ((guess == correct) ? (float).01 : 0);
             Console.WriteLine("Correct: " + correct + " Guess: " + guess + " Correct? " + (guess == correct ? "1 " : "0 ") + "Certainty: " + Math.Round(certainty, 5).ToString().PadRight(7)
-                + " % Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg gradient: " + Math.Round(nn.AvgGradient, 15));
+                + " % Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg error: " + Math.Round(avgerror, 5) + " Avg gradient: " + Math.Round(nn.AvgGradient, 15));
             nn.Dispose();
             isrunning = false;
         }

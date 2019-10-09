fe2d3e1c52719ad64fa08b88a363f8426088b3e9
UnknownDrone097 <48077045+UnknownDrone097@users.noreply.github.com>
2019-10-09 15:20:50 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace Digits
     {
         public static bool isrunning = false;
         public static bool finished = false;
+        static double SaveState = 100;
         static double avg = 1;
         static double maxavg = 0;
         static double avgerror = 1;
@@ -36,34 +37,41 @@ namespace Digits
         }
         public static void program()
         {
-            NN nn = new NN();
-            D.ReadWeightBias(nn);
-            for (int i = 0; i < batchsize - 1; i++)
+            NN nn = new NN(); double[,] image; int correct = -1;
+            //Run the program SaveState times, then save and print out values for that point
+            for (int j = 0; j < SaveState; j++)
             {
-                nn.backprop(Reader.ReadNextImage(), Reader.ReadNextLabel());
-                nn.Descend();
+                D.ReadWeightBias(nn);
+                for (int i = 0; i < batchsize - 1; i++)
+                {
+                    nn.backprop(Reader.ReadNextImage(), Reader.ReadNextLabel());
+                    nn.Descend();
+                }
+                image = Reader.ReadNextImage();
+                correct = Reader.ReadNextLabel();
+                nn.backprop(image, correct);
+                nn.Descend(batchsize);
+                //Print out various things
+                int guess = 0; double certainty = 0;
+                //yeah, yeah, this SHOULD be a NN method, not public and used here but I'm tired right now
+                for (int i = 0; i < 10; i++) { if (nn.OutputValues[i] > certainty) { certainty = nn.OutputValues[i]; guess = i; } }
+                double error = 0;
+                for (int i = 0; i < NN.OutputCount; i++)
+                {
+                    error += ((i == correct ? 1d : 0d) - nn.OutputValues[i]) * ((i == correct ? 1d : 0d) - nn.OutputValues[i]);
+                }
+                avgerror = ((iterator / (iterator + 1)) * avgerror) + ((1 / iterator) * error);
+                avg = (avg * (iterator / (iterator + 1))) + ((guess == correct) ? (1 / iterator) : 0d);
+                iterator++;
+                //Some safety code which is currently disabled
+                //if (avgerror > maxavg && iterator > 300) { maxavg = avgerror; }
+                //if (avgerror > maxavg * 10 && iterator > 300) { finished = true; }
+                Console.WriteLine("Correct: " + correct + " Guess: " + guess + " Correct? " + (guess == correct ? "1 " : "0 ") + "Certainty: " + Math.Round(certainty, 5).ToString().PadRight(7)
+                    + " %Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg error: " + Math.Round(avgerror, 5).ToString().PadRight(8) + " Avg gradient: " + nn.AvgGradient, 15);
             }
-            double[,] image = Reader.ReadNextImage();
-            int correct = Reader.ReadNextLabel();
-            nn.backprop(image, correct);
-            nn.Descend(batchsize);
+            //Save weights and biases
             D.WriteWeightBias(nn);
-            int guess = 0; double certainty = 0;
-            //yeah, yeah, this SHOULD be a NN method, not public and used here but I'm tired right now
-            for (int i = 0; i < 10; i++) { if (nn.Outputs[i].value > certainty) { certainty = nn.Outputs[i].value; guess = i; } }
-            double error = 0;
-            for (int i = 0; i < NN.Count; i++)
-            {
-                error += ((i == correct ? 1d : 0d) - nn.Outputs[i].value) * ((i == correct ? 1d : 0d) - nn.Outputs[i].value);
-            }
-            avgerror = ((iterator / (iterator + 1)) * avgerror) + ((1 / iterator) * error);
-            avg = (avg * (iterator / (iterator + 1))) + ((guess == correct) ? (1 / iterator) : 0d);
-            if (avgerror > maxavg && iterator > 300) { maxavg = avgerror; }
-            if (avgerror > maxavg * 10 && iterator > 300) { finished = true; }
-            Console.WriteLine("Correct: " + correct + " Guess: " + guess + " Correct? " + (guess == correct ? "1 " : "0 ") + "Certainty: " + Math.Round(certainty, 5).ToString().PadRight(7)
-                + " %Correct: " + Math.Round(avg, 5).ToString().PadRight(7) + " Avg error: " + Math.Round(avgerror, 5).ToString().PadRight(8) + " Avg gradient: " + nn.AvgGradient, 15);
             nn.Dispose();
-            iterator++;
             if (iterator > 1000) { iterator = 100; }
             isrunning = false;
         }
